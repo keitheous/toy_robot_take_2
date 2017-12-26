54aefb81eeb1e5f6af53a3b6208d78d84c2efd79
keitheous <keithchongwy@gmail.com>
2017-12-26 20:05:01 +0800

    customizable x,y board

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,8 @@
 class Board
-  attr_reader :max_x
+  attr_reader :max_x, :max_y
 
-  def initialize(max_x = 5)
+  def initialize(max_x = 5, max_y = 5)
     @max_x = max_x
+    @max_y = max_y
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,15 +3,28 @@ require 'spec_helper'
 
 RSpec.describe Board do
   describe ".initialize" do
-    context 'creating instance without params' do
-      it 'returns 5 as maximum X navigation axis' do
-        board = Board.new
+    context 'creating instance without max_x and max_y limit params' do
+      board = Board.new
 
+      it 'returns 5 as maximum X navigation axis' do
         expect(board.max_x).to eq(5)
+      end
+
+      it 'returns 5 as maximum Y navigation axis' do
+        expect(board.max_y).to eq(5)
       end
     end
 
-    # context 'creating board instance with params' do
-    # end
+    context 'creating board instance with max_x and max_y limit params' do
+      board = Board.new(10, 10)
+
+      it 'returns 10 as maximum X navigation axis' do
+        expect(board.max_x).to eq(10)
+      end
+
+      it 'returns 10 as maximum Y navigation axis' do
+        expect(board.max_y).to eq(10)
+      end
+    end
   end
 end
