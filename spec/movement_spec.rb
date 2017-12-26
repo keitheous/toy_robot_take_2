4978f21823db25b047c719bbe0117b6a39383e5b
keitheous <keithchongwy@gmail.com>
2017-12-26 23:06:58 +0800

    case statement tested

--- spec/movement_spec.rb
+++ spec/movement_spec.rb
@@ -12,13 +12,43 @@ RSpec.describe Movement do
   end
 
   describe '.step_forward' do
-    context 'while facing north on 1,1' do
+    context 'while facing north on (1,1)' do
       it 'moves forward to 1,2' do
         robot = Movement.new(1,1, 'north')
         robot.step_forward
 
         expect(robot.x_coordinate).to eq(1)
-        expect(robot.y_coordinate).to eq(1)
+        expect(robot.y_coordinate).to eq(2)
+      end
+    end
+
+    context 'while facing south on (10,10)' do
+      it 'moves forward to 10,9' do
+        robot = Movement.new(10,10, 'south')
+        robot.step_forward
+
+        expect(robot.x_coordinate).to eq(10)
+        expect(robot.y_coordinate).to eq(9)
+      end
+    end
+
+    context 'while facing east on (5,4)' do
+      it 'moves forward to 5,4' do
+        robot = Movement.new(5,4, 'east')
+        robot.step_forward
+
+        expect(robot.x_coordinate).to eq(4)
+        expect(robot.y_coordinate).to eq(4)
+      end
+    end
+
+    context 'while facing west on (5,4)' do
+      it 'moves forward to 5,4' do
+        robot = Movement.new(5,4, 'west')
+        robot.step_forward
+
+        expect(robot.x_coordinate).to eq(6)
+        expect(robot.y_coordinate).to eq(4)
       end
     end
   end
