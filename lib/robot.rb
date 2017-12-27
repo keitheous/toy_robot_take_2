1c7ba7a4f860494fc811a6be7076d780459748a4
keitheous <keithchongwy@gmail.com>
2017-12-27 16:52:48 +0800

    integration position into robot

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,9 +1,10 @@
+require 'position'
+
 class Robot
-  attr_reader :position_x, :position_y, :bearing, :placement
+  attr_reader :position, :bearing, :placement
 
   def initialize(position_x, position_y, bearing, placement = false)
-    @position_x = position_x
-    @position_y = position_y
+    @position = Position.new(position_x, position_y)
     @bearing = bearing
     @placement = placement
   end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,13 +2,25 @@ require 'spec_helper'
 
 RSpec.describe Robot do
   describe '.initialize' do
-    it 'creates an instance with position, bearing, placement' do
-      robot = Robot.new(0, 0, 'north', true)
+    context 'creating a Robot class instance' do
+      let(:robot) { Robot.new(0, 0, 'north', true) }
 
-      expect(robot.position_x).to eq(0)
-      expect(robot.position_y).to eq(0)
-      expect(robot.bearing).to eq('north')
-      expect(robot.placement).to eq(true)
+      it 'contains position object with x y coordinate' do
+        expect(
+          robot.position.instance_of?(Position)
+        ).to eq(true)
+
+        expect(robot.position.x).to eq(0)
+        expect(robot.position.y).to eq(0)
+      end
+
+      it 'contains string to indicate bearing' do
+        expect(robot.bearing).to eq('north')
+      end
+
+      it 'contains placement boolean' do
+        expect(robot.placement).to eq(true)
+      end
     end
   end
 end
