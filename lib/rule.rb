43bbdc66c2e754596201c14a2a0a5c7e16617e3c
keitheous <keithchongwy@gmail.com>
2017-12-27 22:29:11 +0800

    about to integrate rule class

--- lib/rule.rb
+++ lib/rule.rb
@@ -1,23 +1,17 @@
 class Rule
-  attr_reader :fulfilled
-
-  def new
-    @fulfilled = true
-  end
-
-  def robot_inbound(robot_coordinate, board)
+  def self.robot_inbound(robot_coordinate, board)
     inbound =
       (0...board.max_x).include?(robot_coordinate.x) &&
       (0...board.max_y).include?(robot_coordinate.y)
 
     puts "robot is out of bound as #{board.to_s}." unless inbound
-    fulfilled = inbound
+    inbound
   end
 
-  def placed_on_board?(robot)
+  def self.placed_on_board?(robot)
     robot_placed = robot.placed
 
     puts "robot is not yet placed" unless robot_placed
-    fulfilled = robot_placed
+    robot_placed
   end
 end
--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe Rule do
   describe ".placed_on_board?" do
     it 'returns true if robot is placed' do
       placed_robot = Robot.new(0,0,'north', true)
-      rule = Rule.new.placed_on_board?(placed_robot)
+      rule = Rule.placed_on_board?(placed_robot)
 
       expect(rule).to eq(true)
     end
 
     it 'returns false if robot is not placed' do
       floating_robot = Robot.new(0,0,'north', false)
-      rule = Rule.new.placed_on_board?(floating_robot)
+      rule = Rule.placed_on_board?(floating_robot)
 
       expect(rule).to eq(false)
     end
@@ -22,14 +22,14 @@ RSpec.describe Rule do
 
     it 'returns true if robot is within board boundaries' do
       robot_position = Position.new(5, 5)
-      rule = Rule.new.robot_inbound(robot_position, board_boundaries)
+      rule = Rule.robot_inbound(robot_position, board_boundaries)
 
       expect(rule).to eq(true)
     end
 
     it 'returns false if robot is within board boundaries' do
       robot_position = Position.new(10, 11)
-      rule = Rule.new.robot_inbound(robot_position, board_boundaries)
+      rule = Rule.robot_inbound(robot_position, board_boundaries)
 
       expect(rule).to eq(false)
     end
