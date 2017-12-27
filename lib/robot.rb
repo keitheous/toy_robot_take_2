5d2424d0e443ff9c1e0967bb793e4fef4dd691a3
keitheous <keithchongwy@gmail.com>
2017-12-27 17:33:37 +0800

    turn left turn right okay

--- lib/robot.rb
+++ lib/robot.rb
@@ -4,13 +4,18 @@ class Robot
   attr_reader :position, :bearing, :placement
 
   def initialize(position_x, position_y, bearing, placement = false)
-    @position = set_position_coordinate(position_x, position_y)
-    @bearing = bearing
     @placement = placement
+
+    set_position_coordinate(position_x, position_y)
+    set_compass_bearing(bearing)
   end
 
   def set_position_coordinate(x, y)
     @position = Position.new(x, y)
+  end
+
+  def set_compass_bearing(bearing)
+    @bearing = bearing
   end
 
   def move_forward
@@ -21,5 +26,19 @@ class Robot
     set_position_coordinate(movement.x_coordinate, movement.y_coordinate)
   end
 
+  def turn_left
+    movement = Movement.new(self.position.x, self.position.y, self.bearing)
 
+    movement.rotate(-1)
+
+    set_compass_bearing(movement.bearing)
+  end
+
+  def turn_right
+    movement = Movement.new(self.position.x, self.position.y, self.bearing)
+
+    movement.rotate(1)
+
+    set_compass_bearing(movement.bearing)
+  end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -58,4 +58,36 @@ RSpec.describe Robot do
       end
     end
   end
+
+  describe '.turn_left' do
+    it 'turns left from bearing north to west' do
+      robot_starting_north = Robot.new(0, 0, 'north', true)
+      robot_starting_north.turn_left
+
+      expect(robot_starting_north.bearing).to eq('west')
+    end
+
+    it 'turns left from bearing south to east' do
+      robot_starting_south = Robot.new(0, 0, 'south', true)
+      robot_starting_south.turn_left
+
+      expect(robot_starting_south.bearing).to eq('east')
+    end
+  end
+
+  describe '.turn_right' do
+    it 'turns right from bearing north to east' do
+      robot_starting_north = Robot.new(0, 0, 'north', true)
+      robot_starting_north.turn_right
+
+      expect(robot_starting_north.bearing).to eq('east')
+    end
+
+    it 'turns right from bearing east to south' do
+      robot_starting_south = Robot.new(0, 0, 'east', true)
+      robot_starting_south.turn_right
+
+      expect(robot_starting_south.bearing).to eq('south')
+    end
+  end
 end
