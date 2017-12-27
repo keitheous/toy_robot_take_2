b1b6696a9376bcd2d3dca99243dcacc06cfe946c
keitheous <keithchongwy@gmail.com>
2017-12-27 16:42:40 +0800

    robot.rb and basic specs

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,2 +1,10 @@
 class Robot
+  attr_reader :position_x, :position_y, :bearing, :placement
+
+  def initialize(position_x, position_y, bearing, placement = false)
+    @position_x = position_x
+    @position_y = position_y
+    @bearing = bearing
+    @placement = placement
+  end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe Robot do
+  describe '.initialize' do
+    it 'creates an instance with position, bearing, placement' do
+      robot = Robot.new(0, 0, 'north', true)
+
+      expect(robot.position_x).to eq(0)
+      expect(robot.position_y).to eq(0)
+      expect(robot.bearing).to eq('north')
+      expect(robot.placement).to eq(true)
+    end
+  end
 end
