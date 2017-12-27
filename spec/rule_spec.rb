91180bc08fd21411e5391d64831caf24f6db2407
keitheous <keithchongwy@gmail.com>
2017-12-27 20:07:28 +0800

    true false if inbound or noooot

--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe Rule do
+
+
   describe ".robot_inbound" do
-    it 'determines if robot is still within board boundaries' do
-      board_boundaries = Board.new(10, 10)
+    let(:board_boundaries) { Board.new(10, 10) }
+
+    it 'returns true if robot is within board boundaries' do
       robot_position = Position.new(5, 5)
       rule = Rule.robot_inbound(robot_position, board_boundaries)
 
       expect(rule).to eq(true)
+    end
+
+    it 'returns false if robot is within board boundaries' do
+      robot_position = Position.new(10, 11)
+      rule = Rule.robot_inbound(robot_position, board_boundaries)
+
+      expect(rule).to eq(false)
     end
   end
 end
